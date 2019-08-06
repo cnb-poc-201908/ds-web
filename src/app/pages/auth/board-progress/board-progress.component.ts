09232bc61e5e52dba311f1683ffdd965ac6156f7
Yan Xia Bu <yanxiab@cn.ibm.com>
2019-08-06 13:26:03 +0800

    update board-progress

--- src/app/pages/auth/board-progress/board-progress.component.ts
+++ src/app/pages/auth/board-progress/board-progress.component.ts
@@ -1,4 +1,6 @@
+import { VirtualScrollerModule } from 'primeng/virtualscroller';
 import { Component, OnInit } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-board-progress',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardProgressComponent implements OnInit {
 
+  lazyCars = [];
+  totalLazyCarsLength: number;
+  timeout: any;
+
+  waitingMaintainList = [
+    {
+      vin: '辽BD1L23',
+      brand: '10001',
+      color: '王斯伟',
+      year: '201907/31 12:45'
+    },
+    {
+      vin: '辽BD1L23',
+      brand: '10001',
+      color: '王斯伟',
+      year: '201907/31 12:45'
+    },
+    {
+      vin: '辽BD1L23',
+      brand: '10001',
+      color: '王斯伟',
+      year: '201907/31 12:45'
+    },
+    {
+      vin: '辽BD1L23',
+      brand: '10001',
+      color: '王斯伟',
+      year: '201907/31 12:45'
+    },
+    {
+      vin: '辽BD1L23',
+      brand: '10001',
+      color: '王斯伟',
+      year: '201907/31 12:45'
+    },
+    {
+      vin: '辽BD1L23',
+      brand: '10001',
+      color: '王斯伟',
+      year: '201907/31 12:45'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    this.totalLazyCarsLength = this.waitingMaintainList.length;
+  }
+
+  loadCarsLazy(event: LazyLoadEvent) {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+
+    this.timeout = setTimeout(() => {
+      this.lazyCars = [];
+      if (this.waitingMaintainList) {
+        this.lazyCars = this.waitingMaintainList.slice(event.first, (event.first + event.rows));
+      }
+    }, 1000);
   }
 
 }
--- src/app/pages/auth/board-progress/board-progress.module.ts
+++ src/app/pages/auth/board-progress/board-progress.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { BoardProgressRoutingModule } from './board-progress-routing.module';
 import { BoardProgressComponent } from './board-progress.component';
+import { FormsModule } from '@angular/forms';
+import { PrimengModule } from 'src/app/modules/primeng.modules';
 
 
 @NgModule({
   declarations: [BoardProgressComponent],
   imports: [
     CommonModule,
-    BoardProgressRoutingModule
+    BoardProgressRoutingModule,
+    FormsModule,
+    PrimengModule,
   ]
 })
 export class BoardProgressModule { }
