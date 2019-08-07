18495196706850db498fc9660b0a72c943c73d6d
Konia Huo <konia.ho@gmail.com>
2019-08-07 18:40:22 +0800

    add retail

--- src/app/pages/auth/retail/retail.module.ts
+++ src/app/pages/auth/retail/retail.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { RetailRoutingModule } from './retail-routing.module';
 import { RetailComponent } from './retail.component';
-
+import { PrimengModule } from 'src/app/modules/primeng.modules';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RetailComponent],
   imports: [
     CommonModule,
-    RetailRoutingModule
+    RetailRoutingModule,
+    PrimengModule,
+    FormsModule
   ]
 })
 export class RetailModule { }
