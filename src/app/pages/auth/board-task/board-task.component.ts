44e0394fc6ecb8691eecbb3ddd2a84efbd537458
Yan Xia Bu <yanxiab@cn.ibm.com>
2019-08-08 18:47:17 +0800

    update board-task

--- src/app/pages/auth/board-task/board-task.component.ts
+++ src/app/pages/auth/board-task/board-task.component.ts
@@ -19,7 +19,8 @@ export class BoardTaskComponent implements OnInit {
   displaySendMessage: boolean = false;
   displayTaskDetail: boolean = false;
   displayDispatch: boolean = false;
-  inputStr: string;
+  inputStr = '';
+  tabFlag = 'CREATED';
 
   constructor(
     public dialogService: DialogService,
@@ -28,19 +29,24 @@ export class BoardTaskComponent implements OnInit {
 
   tickets: any[];
   dataList: any[];
+  dataListObj = {
+    CREATED: [],
+    INPROGRESS: [],
+    CHECKEDOUT: [],
+  };
 
   ngOnInit() {
     this.items = [
-      { label: '待派工', icon: 'fa fa-fw fa-bar-chart' },
+      { label: '待派工', icon: 'fa fa-fw fa-bar-chart', target: 'CREATED' },
       // { label: '待维修', icon: 'fa fa-fw fa-calendar' },
-      { label: '增项', icon: 'fa fa-fw fa-book' },
-      { label: '待交车', icon: 'fa fa-fw fa-book' },
+      { label: '增项', icon: 'fa fa-fw fa-book', target: 'INPROGRESS' },
+      { label: '待交车', icon: 'fa fa-fw fa-book', target: 'CHECKEDOUT' },
     ];
 
     this.activeIndex = 0;
     this.activeItem = this.items[0];
-    // this.getData("");
-    this.getMockData("");
+    this.getData('');
+    // this.getMockData("");
 
     // this.dispatch({});
   }
@@ -49,36 +55,30 @@ export class BoardTaskComponent implements OnInit {
     // this.rest.getBoardTaskList().subscribe(res=>{
     this.rest.getBoardProgressList().subscribe(res => {
       if (res.code === 200) {
-        if (status && status != "") {
-          this.dataList = res.data.filter(item => {
-            // this.dataList = res.data.filter(item=>{
-            return item.status == status && item.checkInDateTime == null;
-          });
-        } else {
-          this.dataList = res.data;
-        }
-        // If item has more than 1 job, it means Hot Job
-        this.dataList.forEach(item=>{
-          if (item.jobs && item.jobs.length > 0) {
-            item.hot = true;
-          } else {
-            item.hot = false;
+        this.dataListObj.CREATED = [];
+        this.dataListObj.INPROGRESS = [];
+        this.dataListObj.CHECKEDOUT = [];
+        res.data.forEach(element => {
+          if (element.status === 'CREATED') {
+            this.dataListObj.CREATED.push(element);
+          } else if (element.status === 'INPROGRESS') {
+            this.dataListObj.INPROGRESS.push(element);
+          } else if (element.status === 'CHECKEDOUT') {
+            this.dataListObj.CHECKEDOUT.push(element);
           }
         });
-
-        console.log(this.dataList);
       }
     });
   }
 
   getMockData(status) {
-    this.rest.getBoardTaskList().subscribe(res=>{
+    this.rest.getBoardTaskList().subscribe(res => {
       if (res.code === 0) {
         if (status && status != "") {
-          this.dataList = res.data.filter(item=>{
+          this.dataList = res.data.filter(item => {
             // this.dataList = res.data.filter(item=>{
-              return item.status == status && item.checkInDateTime == null;
-            });
+            return item.status == status && item.checkInDateTime == null;
+          });
         } else {
           this.dataList = res.data;
         }
@@ -91,7 +91,6 @@ export class BoardTaskComponent implements OnInit {
           }
         });
 
-        console.log(this.dataList);
       }
     });
   }
@@ -99,6 +98,7 @@ export class BoardTaskComponent implements OnInit {
   change(event, index, item) {
     this.activeIndex = index;
     this.activeItem = item;
+    this.tabFlag = item.target;
   }
 
   dispatch(item) {
@@ -138,27 +138,23 @@ export class BoardTaskComponent implements OnInit {
     this.displayTaskDetail = false;
   }
 
-  search() {
+  search(inputStr) {
     if (this.inputStr === '') {
-      // this.getData('');
-      this.getMockData('');
+      this.getData('');
+      // this.getMockData('');
     } else {
       this.rest.searchCar(this.inputStr).subscribe(res => {
         if (res.code === 200) {
-          if (status && status !== '') {
-            this.dataList = res.data.filter(item => {
-              // this.dataList = res.data.filter(item=>{
-              return item.status === status && item.checkInDateTime == null;
-            });
-          } else {
-            this.dataList = res.data;
-          }
-          // If item has more than 1 job, it means Hot Job
-          this.dataList.forEach(item => {
-            if (item.jobs && item.jobs.length > 0) {
-              item.hot = true;
-            } else {
-              item.hot = false;
+          this.dataListObj.CREATED = [];
+          this.dataListObj.INPROGRESS = [];
+          this.dataListObj.CHECKEDOUT = [];
+          res.data.forEach(element => {
+            if (element.status === 'CREATED') {
+              this.dataListObj.CREATED.push(element);
+            } else if (element.status === 'INPROGRESS') {
+              this.dataListObj.INPROGRESS.push(element);
+            } else if (element.status === 'CHECKEDOUT') {
+              this.dataListObj.CHECKEDOUT.push(element);
             }
           });
         }
