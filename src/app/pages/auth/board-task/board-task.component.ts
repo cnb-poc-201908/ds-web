e39a3f4cefc9f6525918e01a6bdf309fb803ea90
wdajun <51494794@qq.com>
2019-08-08 11:05:49 +0800

    add tech-select

--- src/app/pages/auth/board-task/board-task.component.ts
+++ src/app/pages/auth/board-task/board-task.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { DialogService } from 'primeng/api';
 import { TechSelect } from './components/tech-select/tech-select';
-
-// import { SelectTechComponent } from 'src/app/components/dialog/select-tech/select-tech.component';
-
 import { RestService } from 'src/app/services/rest.service';
 
 @Component({
@@ -39,106 +36,18 @@ export class BoardTaskComponent implements OnInit {
       { label: '待交车', icon: 'fa fa-fw fa-book' },
     ];
 
-    // this.tickets = [
-    //   {
-    //     vin: 'XLB09930LB1233',
-    //     serial : '辽BCF916',
-    //     totalHours : 9,
-    //     expact : '2019/07/31 12:00',
-    //     submit : '2019/07/31 12:45',
-    //     model : 'BMW0011',
-    //     sa : '王经理',
-    //     remark : '钣金喷漆',
-    //     hot : true,
-    //   },
-    //   {
-    //     vin: 'XLB09930LB1311',
-    //     serial : '辽BCF912',
-    //     totalHours : 3,
-    //     expact : '2019/07/31 12:00',
-    //     submit : '2019/07/31 12:45',
-    //     model : 'BMW0011',
-    //     sa : '王经理',
-    //     remark : '钣金喷漆',
-    //     hot : false,
-    //   },
-    //   {
-    //     vin: 'XLB09930XT9125',
-    //     serial : '辽BCN115',
-    //     totalHours : 13,
-    //     expact : '2019/07/31 12:00',
-    //     submit : '2019/07/31 12:45',
-    //     model : 'BMW0011',
-    //     sa : '王经理',
-    //     remark : '钣金喷漆',
-    //     hot : true,
-    //   },
-    //   {
-    //     vin: 'XLB09938X3125J',
-    //     serial : '辽BC8012',
-    //     totalHours : 15,
-    //     expact : '2019/07/31 12:00',
-    //     submit : '2019/07/31 12:45',
-    //     model : 'BMW0011',
-    //     sa : '王经理',
-    //     remark : '钣金喷漆',
-    //     hot : false,
-    //   },
-    //   {
-    //     vin: 'XBR99930LB1233',
-    //     serial : '辽BCP031',
-    //     totalHours : 19,
-    //     expact : '2019/07/31 12:00',
-    //     submit : '2019/07/31 12:45',
-    //     model : 'BMW0011',
-    //     sa : '王经理',
-    //     remark : '钣金喷漆',
-    //     hot : true,
-    //   },
-    //   {
-    //     vin: 'XUI34430LB1903',
-    //     serial : '辽BCF234',
-    //     totalHours : 4,
-    //     expact : '2019/07/31 12:00',
-    //     submit : '2019/07/31 12:45',
-    //     model : 'BMW0011',
-    //     sa : '王经理',
-    //     remark : '钣金喷漆',
-    //     hot : false,
-    //   },
-    //   {
-    //     vin: 'XLB09930LB1233',
-    //     serial : '辽BC0A12',
-    //     totalHours : 8,
-    //     expact : '2019/07/31 12:00',
-    //     submit : '2019/07/31 12:45',
-    //     model : 'BMW0011',
-    //     sa : '王经理',
-    //     remark : '钣金喷漆',
-    //     hot : false,
-    //   },
-    //   {
-    //     vin: 'XLB09930LB1233',
-    //     serial : '辽BH3934',
-    //     totalHours : 2,
-    //     expact : '2019/07/31 12:00',
-    //     submit : '2019/07/31 12:45',
-    //     model : 'BMW0011',
-    //     sa : '王经理',
-    //     remark : '钣金喷漆',
-    //     hot : true,
-    //   },
-    // ];
-
     this.activeIndex = 0;
     this.activeItem = this.items[0];
     this.getData("CREATED");
+
+    this.dispatch({});
   }
 
   getData(status) {
-    this.rest.getBoardProgressList().subscribe(res=>{
+    this.rest.getBoardTaskList().subscribe(res=>{
       if (res.code === 0) {
-        this.dataList = res.data.items.filter(item=>{
+        // this.dataList = res.data.items.filter(item=>{
+        this.dataList = res.data.filter(item=>{
           return item.status == status && item.checkInDateTime == null;
         });
 
@@ -164,8 +73,8 @@ export class BoardTaskComponent implements OnInit {
   dispatch(item) {
     const ref = this.dialogService.open(TechSelect, {
       header: '派工',
-      width: '70%',
-      height: '60%',
+      width: '80%',
+      height: '70%',
       baseZIndex: 10000
     });
 
